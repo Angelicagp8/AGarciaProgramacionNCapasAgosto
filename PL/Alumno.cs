ca25f7b8dbd7dfd061ca161732b94c8606c5fca3
Carmen Angélica García Pérez <cangelicagp@gmail.com>
2022-08-17 16:25:12 -0500

    Add project files.

---
+++ PL/Alumno.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Alumno
+    {
+        public static void Add()
+        {
+            ML.Alumno alumno = new ML.Alumno();  //CREAR OBJETO // INSTANCIAMOS PARA PODER ACCEDER A LOS MÉTODOS Y ATRIBUTOS
+
+            Console.WriteLine("Ingrese el nombre");
+            alumno.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el  ApellidoPaterno");
+            alumno.ApellidoPaterno = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el ApellidoMaterno");
+            alumno.ApellidoMaterno = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el Sexo");
+            alumno.Sexo = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el email");
+            alumno.Email = Console.ReadLine();
+
+            ML.Result result = BL.Alumno.Add(alumno); 
+
+            if(result.Correct)
+            {
+                Console.WriteLine("Registro exitoso");
+            }
+            else
+            {
+                Console.WriteLine("Ocurrio un error al insertar el registro" );
+            }
+        }
+    }
+}
