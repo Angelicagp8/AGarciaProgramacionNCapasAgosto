c00592feb15f801a66f0fbc0f0f7ad943a164c02
Carmen Angélica García Pérez <cangelicagp@gmail.com>
2022-08-30 11:46:29 -0500

    Add MVC

--- PL_MVC/Controllers/AlumnoController.cs
+++ PL_MVC/Controllers/AlumnoController.cs
@@ -21,5 +21,48 @@ namespace PL_MVC.Controllers
             }
             return View(alumno); //ACTION RESULT : TIpos de retorno que tenemos en los métodos EJEMPLO : ActionResult -> Vista en HTML // JsonResult -> Objeto de tipo JSON  para responder a peticiones AJAX
         }
+
+        [HttpGet]
+        public ActionResult Form(int? IdAlumno)
+        {
+            ML.Alumno alumno = new ML.Alumno();
+            alumno.Semestre = new ML.Semestre();
+            if(IdAlumno == null)
+            {
+                return View(alumno);
+            }
+            else
+            {
+                //Formulario precargado con los datos del registro GETBYID
+            }
+
+            return View();
+
+        }
+        [HttpPost] 
+        public ActionResult Form(ML.Alumno alumno)
+        {
+            ML.Result result = new ML.Result();
+
+            if(alumno.IdAlumno == 0)
+            {
+                result = BL.Alumno.AddEF(alumno);
+
+                if(result.Correct)
+                {
+                    ViewBag.Mensaje = "Registro exitoso";
+                }
+                else
+                {
+                    ViewBag.Mensaje = "Ocurrio un error al insertar el registro" + result.ErrorMessage;
+                }
+            }
+            else
+            {
+                //UPDATE
+            }
+            return PartialView("Modal");
+        }
+
     }
 }
